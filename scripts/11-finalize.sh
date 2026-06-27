a0c88dcb60827a1cafe49b6ddd00b8ca794ff2bb
William <126926699+GitXpresso@users.noreply.github.com>
2026-06-27 18:49:34 -0400

    Update 11-finalize.sh
    Added Support for qdbus

--- scripts/11-finalize.sh
+++ scripts/11-finalize.sh
@@ -114,7 +114,12 @@ read -p "Would you like to log out now? (y/N): " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
         echo "Logging out..."
-        qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout 2>/dev/null
+        if command -v qdbus6; then
+          qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout 2>/dev/null
+        fi
+        if command -v qdbus; then
+          qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout 2>/dev/null
+        fi
         ;;
     *)
         echo "Exiting script. Please remember to log out manually later."
